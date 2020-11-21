6052f1b12441674ff59362e79be32e7232916407
Alex Hutman <alexhutman@gmail.com>
2020-11-20 22:21:53 -0500

    added possible commands for the protocol

--- picontrol_common.h
+++ picontrol_common.h
@@ -27,4 +27,19 @@
 // the others to get lost if there is an existing open connection.
 
 
+// Types
+typedef enum {
+	PI_CTRL_ACK,        // Server: You are now connected - continue
+	PI_CTRL_BUSY,       // Server: Someone is already connected - you must disconnect
+
+	PI_CTRL_SET_NAME,   // Client: Send current name so server can say who is currently connected on a PI_CTRL_BUSY
+	PI_CTRL_MOUSE_MV,   // Client: Send x,y of relative position to move mouse to
+	PI_CTRL_MOUSE_DOWN, // Client: Say to press mouse down (no data required)
+	PI_CTRL_MOUSE_UP,   // Client: Say to press mouse up (no data required)
+	PI_CTRL_MOUSE_CLK,  // Client: Say to click (mouseup, then mousedown) mouse (no data required)
+	PI_CTRL_KEY_PRESS,  // Client: Send UTF-8 value of key to be pressed (details TBD)
+} PiControlCmd;
+
+
+
 #endif
