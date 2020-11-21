d55afca282699d558213c2807515c94a6bc13d15
Alex Hutman <alexhutman@gmail.com>
2020-11-20 20:14:07 -0500

    updated test script to include size of message

--- test_emit_client_msg.sh
+++ test_emit_client_msg.sh
@@ -1,1 +1,11 @@
-printf "Hello, this is a test! Carry on." | netcat localhost 14741
+#!/usr/bin/env bash
+
+ADDR=localhost
+PORT=14741
+
+msg="Hello, this is a test! Carry on."
+msg_byte_len=${#msg}
+msg_byte_len_hex=$( printf "%x" $msg_byte_len )
+
+printf "Message: |%s|\nIt is %s = 0x%s bytes long.\n" "$msg" "$msg_byte_len" "$msg_byte_len_hex"
+printf "%b%s" "\x$msg_byte_len_hex" "$msg" | netcat $ADDR $PORT
