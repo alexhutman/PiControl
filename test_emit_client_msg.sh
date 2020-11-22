467006482f5739589770bbf38b53b2fbc8b7fc5d
Alex Hutman <alexhutman@gmail.com>
2020-11-21 21:33:38 -0500

    test script is now more robust/helpful

--- test_emit_client_msg.sh
+++ test_emit_client_msg.sh
@@ -3,9 +3,15 @@
 ADDR=localhost
 PORT=14741
 
-msg="Hello, this is a test! Carry on."
+# Input test commands/messages here
+# Testing a SET_NAME command for example
+cmd=5
+msg="Alex's iPhone"
+
+cmd_hex=$( printf "%x" $cmd )
 msg_byte_len=${#msg}
 msg_byte_len_hex=$( printf "%x" $msg_byte_len )
 
 printf "Message: |%s|\nIt is %s = 0x%s bytes long.\n" "$msg" "$msg_byte_len" "$msg_byte_len_hex"
-printf "%b%s" "\x$msg_byte_len_hex" "$msg" | netcat $ADDR $PORT
+printf "Command: 0x%s\n" "$cmd_hex"
+printf "%b%b%s" "\x$msg_byte_len_hex" "\x$cmd_hex" "$msg" | netcat $ADDR $PORT
