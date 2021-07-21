239b4df6b836459501f149df5a553b3e80e64f07
Alex Hutman <alexhutman@gmail.com>
2021-07-20 23:32:40 -0400

    removed extraneous includes

--- picontrol_server.c
+++ picontrol_server.c
@@ -5,22 +5,14 @@
  */
 
 // Includes
-#include <arpa/inet.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
-#include <netdb.h>
 #include <netinet/in.h>
-#include <net/if.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include <xdo.h>
 
