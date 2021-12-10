81224f4b856703e7ec339413a58c4bff7d90a713
Alex <alexhutman@gmail.com>
2021-12-10 00:24:07 -0500

    removed 'static' in uinput functions

--- picontrol_uinput.h
+++ picontrol_uinput.h
@@ -10,8 +10,8 @@ extern const int len_ascii_to_scancodes;
 
 int picontrol_create_uinput_fd();
 int picontrol_destroy_uinput_fd(int fd);
-static ssize_t picontrol_emit(int fd, int type, int code, int val);
-static void picontrol_type_char(int fd, char c);
+ssize_t picontrol_emit(int fd, int type, int code, int val);
+void picontrol_type_char(int fd, char c);
 void picontrol_print_str(int fd, char *str);
 
 #endif
