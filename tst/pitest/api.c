8eba8a9d996d03c41185fc8ee5367272b7e16941
Alex <alexhutman@gmail.com>
2024-01-27 20:04:21 -0500

    Made PiTest API a bit easier to use

--- tst/pitest/api.c
+++ tst/pitest/api.c
@@ -13,14 +13,76 @@ int run_test(const TestCase *test_case) {
     return 0;
 }
 
-size_t run_test_suite(const TestCase test_cases[], size_t num_tests) {
+int run_setup_if_exists(SetupFunction setup) {
+    if (setup == NULL) {
+        return 0;
+    }
+    return setup();
+}
+
+int run_teardown_if_exists(TeardownFunction teardown) {
+    if (teardown == NULL) {
+        return 0;
+    }
+    return teardown();
+}
+
+size_t run_test_suite(const TestSuite *suite) {
+    if (suite->num_tests == 0) {
+        return 0;
+    }
+    pictrl_log_info("Running test suite '%s'\n", suite->name);
+
+    const SetupFunction suite_setup = suite->before_after_all.setup;
+    int suite_setup_ret;
+    if ((suite_setup_ret = run_setup_if_exists(suite_setup)) != 0) {
+        pictrl_log_error("Setup for suite '%s' failed with code %d\n",
+                         suite->name, suite_setup_ret);
+        return 1;
+    }
+
     size_t failed_test_count = 0;
-    for (size_t test_id = 0; test_id < num_tests; test_id++) {
-        int res = run_test(&test_cases[test_id]);
-        if (res != 0) {
+    for (size_t test_id = 0; test_id < suite->num_tests; test_id++) {
+        const SetupFunction case_setup = suite->before_after_each.setup;
+        int case_setup_ret;
+        if ((case_setup_ret = run_setup_if_exists(case_setup)) != 0) {
+            pictrl_log_error("Setup for test case '%s' failed with code %d\n. Skipping...",
+                             suite->test_cases[test_id].test_name, case_setup_ret);
+            failed_test_count++;
+            continue;
+        }
+
+        bool test_failed = run_test(&suite->test_cases[test_id]) != 0;
+        if (test_failed) {
+            failed_test_count++;
+        }
+
+        const TeardownFunction case_teardown = suite->before_after_each.teardown;
+        int case_teardown_ret;
+        if ((case_teardown_ret = run_teardown_if_exists(case_teardown)) != 0) {
+            pictrl_log_error("Teardown for test case '%s' failed with code %d\n",
+                             suite->test_cases[test_id].test_name, case_teardown_ret);
+            if (!test_failed) {
+                failed_test_count++;
+            }
+        }
+    }
+
+    const TestFunction suite_teardown = suite->before_after_all.teardown;
+    int suite_teardown_ret;
+    if ((suite_teardown_ret = run_teardown_if_exists(suite_teardown)) != 0) {
+        pictrl_log_error("Teardown for suite '%s' failed with code %d\n",
+                         suite->name, suite_teardown_ret);
+        if (failed_test_count == 0) {
             failed_test_count++;
         }
     }
 
-    return failed_test_count;
+    if (failed_test_count > 0) {
+        pictrl_log_error("Test suite '%s' failed!\n", suite->name);
+        return failed_test_count;
+    }
+
+    pictrl_log_info("Test suite '%s' passed!\n", suite->name);
+    return 0;
 }
--- tst/pitest/api.h
+++ tst/pitest/api.h
@@ -6,14 +6,29 @@
 
 #define pictrl_size(arr) sizeof(arr)/sizeof(arr[0])
 
-// Function pointer: () -> int
+// Function pointer: () -> int (they're all currently the same but don't have to be)
 typedef int (*TestFunction)();
+typedef int (*SetupFunction)();
+typedef int (*TeardownFunction)();
 
-typedef struct TestCase {
+typedef struct {
+    const SetupFunction setup;
+    const TeardownFunction teardown;
+} SetupTeardown;
+
+typedef struct {
     const char *test_name;
     const TestFunction test_function;
 } TestCase;
 
+typedef struct {
+    const char *name;
+    const TestCase *test_cases;
+    const size_t num_tests;
+    const SetupTeardown before_after_all;
+    const SetupTeardown before_after_each;
+} TestSuite;
+
 int run_test(const TestCase*);
-size_t run_test_suite(const TestCase test_cases[], size_t num_tests);
+size_t run_test_suite(const TestSuite*);
 #endif
