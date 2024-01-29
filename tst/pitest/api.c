8bacf50b6c6f6d4c81f32c7f3d4348e48f9f5d47
Alex <alexhutman@gmail.com>
2024-01-28 23:42:08 -0500

    Fixed tomfoolery
    ...idk wth I was thinking either

--- tst/pitest/api.c
+++ tst/pitest/api.c
@@ -34,8 +34,8 @@ size_t run_test_suite(const TestSuite *suite) {
     pictrl_log_info("Running test suite '%s'\n", suite->name);
 
     const SetupFunction suite_setup = suite->before_after_all.setup;
-    int suite_setup_ret;
-    if ((suite_setup_ret = run_setup_if_exists(suite_setup)) != 0) {
+    int suite_setup_ret = run_setup_if_exists(suite_setup);
+    if (suite_setup_ret != 0) {
         pictrl_log_error("Setup for suite '%s' failed with code %d\n",
                          suite->name, suite_setup_ret);
         return 1;
@@ -44,8 +44,8 @@ size_t run_test_suite(const TestSuite *suite) {
     size_t failed_test_count = 0;
     for (size_t test_id = 0; test_id < suite->num_tests; test_id++) {
         const SetupFunction case_setup = suite->before_after_each.setup;
-        int case_setup_ret;
-        if ((case_setup_ret = run_setup_if_exists(case_setup)) != 0) {
+        int case_setup_ret = run_setup_if_exists(case_setup);
+        if (case_setup_ret != 0) {
             pictrl_log_error("Setup for test case '%s' failed with code %d\n. Skipping...",
                              suite->test_cases[test_id].test_name, case_setup_ret);
             failed_test_count++;
@@ -58,8 +58,8 @@ size_t run_test_suite(const TestSuite *suite) {
         }
 
         const TeardownFunction case_teardown = suite->before_after_each.teardown;
-        int case_teardown_ret;
-        if ((case_teardown_ret = run_teardown_if_exists(case_teardown)) != 0) {
+        int case_teardown_ret = run_teardown_if_exists(case_teardown);
+        if (case_teardown_ret != 0) {
             pictrl_log_error("Teardown for test case '%s' failed with code %d\n",
                              suite->test_cases[test_id].test_name, case_teardown_ret);
             if (!test_failed) {
@@ -69,8 +69,8 @@ size_t run_test_suite(const TestSuite *suite) {
     }
 
     const TestFunction suite_teardown = suite->before_after_all.teardown;
-    int suite_teardown_ret;
-    if ((suite_teardown_ret = run_teardown_if_exists(suite_teardown)) != 0) {
+    int suite_teardown_ret = run_teardown_if_exists(suite_teardown);
+    if (suite_teardown_ret != 0) {
         pictrl_log_error("Teardown for suite '%s' failed with code %d\n",
                          suite->name, suite_teardown_ret);
         if (failed_test_count == 0) {
