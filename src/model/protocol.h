982139d3dd9490b184227ba7c1c213dc4bc22b80
Alex <alexhutman@gmail.com>
2024-08-06 22:22:02 -0400

    [model/protocol.h] Removed extraneous comments

--- src/model/protocol.h
+++ src/model/protocol.h
@@ -5,13 +5,9 @@
 
 typedef enum {
   PI_CTRL_HEARTBEAT,  // Client: Send heartbeat so server can disconnect if
-                      // connection is lost
-
-  PI_CTRL_MOUSE_MV,  // Client: Send x,y of relative position to move mouse to
+                      //         connection is lost
+  PI_CTRL_MOUSE_MV,   // Client: Send x,y of relative position to move mouse to
   PI_CTRL_MOUSE_CLICK,  // Client: Say to click (mouseup or mousedown) mouse
-                        // (left or right button)
-                        //   This is 1 byte, where 00000021 the 2 ==
-                        //   PiCtrlMouseBtn and the 1 == PiCtrlMouseClick
   PI_CTRL_TEXT,         // Client: Send UTF-8 bytes to be typed
   PI_CTRL_KEYSYM,       // Client: Send keysym (combination)
 } PiCtrlCmd;
